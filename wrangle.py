6b82fc53b11a3ad634af7e979ac88d2f35e67630
JenniferMLewis <lewis.jennifer.marie.15@gmail.com>
2022-11-03 13:20:23 -0500

    More things, not all useful, but they're things tried.

--- wrangle.py
+++ wrangle.py
@@ -73,7 +73,8 @@ def prep_bees():
     df['colonies_net_gain'] = df.ending_colonies - df.starting_colonies
     # create a column for beekeeper to colony ratio
     df['beekeeper_colony_ratio'] = df.ending_colonies / df.beekeepers
-    
+    # look at only beekeepers exclusive to state
+    df = df[df.beekeepers_exclusive_to_state == 100]
     # return the cleaned and sorted dataframe
     return df
 
@@ -120,6 +121,7 @@ def bee_merged():
     
     #return back dataframe
     return df
+
 
 def bee_wrangle():
     '''
